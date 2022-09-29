501e397cfcac40f949bd334530edc89eb944c191
Akv0603 <akv060388@gmail.com>
2022-09-29 16:13:03 +0700

    ++fatJar common

---
+++ common/src/main/java/jdev/dto/Main.java
@@ -0,0 +1,10 @@
+package jdev.dto;
+
+/**
+ * Created by User on 029 29.09.22.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello common Main.main");
+    }
+}
